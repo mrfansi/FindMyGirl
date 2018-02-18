6b939675d13e8e41cf05cf4b1a66594076a4c7c9
Muhammad Irfan <41815110165@student.mercubuana.ac.id>
2018-02-18 21:48:39 +0700

    Update

--- app/src/main/java/eazy_dev/findmygirl/Login.java
+++ app/src/main/java/eazy_dev/findmygirl/Login.java
@@ -14,6 +14,6 @@ public class Login extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
-        // test
+
     }
 }
--- app/src/main/java/eazy_dev/findmygirl/Splash.java
+++ app/src/main/java/eazy_dev/findmygirl/Splash.java
@@ -6,10 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class Splash extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 4000;
+
+    ImageView iv_logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,15 @@ public class Splash extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        Random r = new Random();
+        iv_logo = findViewById(R.id.iv_logo);
+        RotateAnimation rotateAnimation = new RotateAnimation(0, r.nextInt(3600) + 360,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setFillAfter(true);
+        rotateAnimation.setDuration(1000);
+        rotateAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        iv_logo.startAnimation(rotateAnimation);
 
 
         new Handler().postDelayed(new Runnable() {
