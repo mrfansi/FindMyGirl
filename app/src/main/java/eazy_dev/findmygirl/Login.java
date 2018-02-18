c99fbe95ce2aa9bbd562785317d655e16af0e77f
Muhammad Irfan <41815110165@student.mercubuana.ac.id>
2018-02-18 22:35:36 +0700

    Update

--- app/src/main/java/eazy_dev/findmygirl/Login.java
+++ app/src/main/java/eazy_dev/findmygirl/Login.java
@@ -1,9 +1,12 @@
 package eazy_dev.findmygirl;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -14,6 +17,17 @@ public class Login extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        Button googleSignIn = findViewById(R.id.btnlogin_google);
+        googleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(Login.this, Home.class);
+                startActivity(homeIntent);
+
+                finish();
+            }
+        });
 
     }
 }
